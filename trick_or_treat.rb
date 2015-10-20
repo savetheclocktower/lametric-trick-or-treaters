b74ae0260c997cf8ed9c467634e009ccbaf8652b
Andrew Dupont <andrew.dupont@bazaarvoice.com>
2015-10-20 11:14:54 -0500

    Stuff.

--- trick_or_treat.rb
+++ trick_or_treat.rb
@@ -1,5 +1,23 @@
 require 'sinatra'
+require 'redis'
+
+$redis = Redis.new( url: ENV['REDIS_URL'] )
+
+KEY = 'trick_or_treaters'
+
+def get_count
+  $redis.get(KEY).to_i
+end
+
+def increment_count
+  $redis.incr(KEY).to_i
+end
 
 get '/' do
   "Hello World!"
+end
+
+get '/increment' do
+  increment_count
+  get_count.to_s
 end

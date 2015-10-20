30f91bd9e10493d86f8cd02e003f3fd332153941
Andrew Dupont <andrew.dupont@bazaarvoice.com>
2015-10-20 11:17:13 -0500

    Add reset.

--- trick_or_treat.rb
+++ trick_or_treat.rb
@@ -13,11 +13,20 @@ def increment_count
   $redis.incr(KEY).to_i
 end
 
+def reset_count
+  $redis.set(KEY, 0)
+end
+
 get '/' do
   "Hello World!"
 end
 
 get '/increment' do
   increment_count
+  get_count.to_s
+end
+
+get '/reset' do
+  reset_count
   get_count.to_s
 end

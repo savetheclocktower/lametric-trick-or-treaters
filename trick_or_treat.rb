5004789a848e9d6b240828bab50743d69df020a6
Andrew Dupont <andrew.dupont@bazaarvoice.com>
2015-10-20 11:24:03 -0500

    LaMetric first pass.

--- trick_or_treat.rb
+++ trick_or_treat.rb
@@ -5,6 +5,32 @@ $redis = Redis.new( url: ENV['REDIS_URL'] )
 
 KEY = 'trick_or_treaters'
 
+
+LAMETRIC_JSON = {
+  frames: [
+    {
+      index: 0,
+      text: "trick",
+      icon: ""
+    },
+    {
+      index: 1,
+      text: "or",
+      icon: ""
+    },
+    {
+      index: 2,
+      text: "treat",
+      icon: ""
+    },
+    {
+      index: 3,
+      text: "served:",
+      icon: ""
+    }
+  ]
+}
+
 def get_count
   $redis.get(KEY).to_i
 end
@@ -29,4 +55,15 @@ end
 get '/reset' do
   reset_count
   get_count.to_s
+end
+
+get '/lametric' do
+  json = JSON.parse( JSON.encode(LAMETRIC_JSON) )
+  json['frames'].push({
+    index: 4,
+    text: get_count.to_s,
+    icon: ""
+  })
+  
+  JSON.encode(json)
 end
